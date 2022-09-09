bafda398cee3d6e4d9aff09b2deb7eefc410c33f
Alex Maraza <alextufielamigo@gmail.com>
2022-09-08 21:51:57 -0400

    segundo commit

--- src/Components/Paginas/Privado/Usuarios/ListaUsuarios.jsx
+++ src/Components/Paginas/Privado/Usuarios/ListaUsuarios.jsx
@@ -1,8 +1,88 @@
+import { AppBar, Button, Container, Grid, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from '@material-ui/core'
+import axios from 'axios'
 import React from 'react'
+import { useEffect, useState } from 'react'
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { useNavigate } from 'react-router-dom';
 
 const ListaUsuarios = () => {
+  const navigation=useNavigate()
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  //------GET USUARIOS-----------
+  const getUser = async () => {
+    await axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(resp => {
+        setUser(resp.data)
+      })
+      .catch(err => console.log(err))
+  }
+  console.log(user)
+
+  const data = [
+    { nombre: 'Maria', apellidoP: 'Mecedes', apellidoM: 'Arce', email: 'maria123@gmail.com' },
+    { nombre: '', apellidoP: 'Mecedes', apellidoM: 'Arce', email: 'maria123@gmail.com' },
+    { nombre: 'Maria', apellidoP: 'Mecedes', apellidoM: 'Arce', email: 'maria123@gmail.com' },
+    { nombre: 'Maria', apellidoP: 'Mecedes', apellidoM: 'Arce', email: 'maria123@gmail.com' },
+    { nombre: 'Maria', apellidoP: 'Mecedes', apellidoM: 'Arce', email: 'maria123@gmail.com' },
+    { nombre: 'Maria', apellidoP: 'Mecedes', apellidoM: 'Arce', email: 'maria123@gmail.com' },
+    { nombre: 'Maria', apellidoP: 'Mecedes', apellidoM: 'Arce', email: 'maria123@gmail.com' },
+    { nombre: 'Maria', apellidoP: 'Mecedes', apellidoM: 'Arce', email: 'maria123@gmail.com' },
+  ]
   return (
-    <div>ListaUsuarios</div>
+    <>
+      <AppBar position='absolute'>
+        <Toolbar>
+          <div style={{flexGrow:1}}></div>
+          <Button onClick={()=>navigation('/')}  style={{ color: 'white'}}>Cerrar Sesion</Button>
+        </Toolbar>
+      </AppBar>
+      <Container fixed style={{ marginTop: 100 }}>
+        <Typography variant='h5' align='center'>Lista de Usuarios</Typography>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Usuario</TableCell>
+                <TableCell>Nombre</TableCell>
+                <TableCell>Correo Electronico</TableCell>
+                <TableCell>Cuidad</TableCell>
+                <TableCell>Telefono</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {user ? (
+                user.map((e, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{e.username}</TableCell>
+                    <TableCell>{e.name}</TableCell>
+                    <TableCell>{e.email}</TableCell>
+                    <TableCell>{e.address.city}</TableCell>
+                    <TableCell>{e.phone}</TableCell>
+                    <TableCell>
+                      <Grid container direction='row' justifyContent='space-evenly'>
+                        <IconButton size='small' style={{ color: 'green' }}>
+                          <EditIcon />
+                        </IconButton>
+                        <IconButton size='small' style={{ color: 'red' }}>
+                          <DeleteIcon />
+                        </IconButton>
+                      </Grid>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (null)}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
+    </>
   )
 }
 
--- src/Components/Paginas/Publico/Inicio.jsx
+++ src/Components/Paginas/Publico/Inicio.jsx
@@ -1,58 +1,90 @@
-import { Grid,Container, Typography, InputLabel, TextField, InputAdornment, Button } from '@material-ui/core'
+import { Grid, Container, Typography, InputLabel, TextField, InputAdornment, Button } from '@material-ui/core'
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import LockIcon from '@material-ui/icons/Lock';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Inicio = () => {
-  return (
-    <Container fixed>
-      <Typography align='center' variant='h5'>Iniciar Sesion</Typography>
-      <Container maxWidth='sm'>
-        <InputLabel style={{ marginBottom: 10 }}>Nombre de Usuario</InputLabel>
-        <TextField
-          style={{ marginBottom: 10 }}
-          variant='outlined'
-          size='small'
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment style={{ paddingRight: 10 }}>
-                <AccountCircleIcon />
-              </InputAdornment>
-            )
-          }}
-        />
-        <InputLabel style={{ marginBottom: 10 }}>Contraseña</InputLabel>
-        <TextField
-          style={{ marginBottom: 10 }}
-          variant='outlined'
-          size='small'
-          type='password'
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment style={{ paddingRight: 10 }}>
-                <LockIcon />
-              </InputAdornment>
-            )
-          }}
-        />
-        <Button fullWidth variant='outlined' style={{ background: '#43a047', color: 'white', marginTop: 10, marginBottom: 10 }}>Iniciar Sesion</Button>
-        <Grid container direction='row' justify='space-between'>
-          <Typography>
-            <Link to='/registro-usuario' style={{ textDecoration: 'none', color: '#0288d1' }}>
-              Registrarse
-            </Link>
-          </Typography>
-          <Typography>
-            <Link to='/password' style={{ textDecoration: 'none', color: '#0288d1' }}>
-              ¿Olvidate tu Contraseña?
-            </Link>
-          </Typography>
-        </Grid>
-      </Container>
+  const navigation = useNavigate()
+  const [changeData, setChangeData] = useState({ user_name: '', user_pass: '' })
 
+  //-------------LOGIN-------------------------
+  const login = (e) => {
+    e.preventDefault()
+    // console.log(changeData)
+    if (changeData.user_name === 'admin' && changeData.user_pass === 'admin') {
+      return navigation('/usuarios')
+    } else {
+      return alert('Contraseña Incorrecta')
+    }
+  }
+
+  //-----------HANLDE CHANGE---------------------
+  const handleChange = (e) => {
+    setChangeData({
+      ...changeData,
+      [e.target.name]: e.target.value
+    })
+  }
+  return (
+    <Container fixed style={{display:'flex',height:'90vh', alignItems:'center',justifyContent:'center'}}>
+      <div >
+        <Typography align='center' variant='h5' style={{marginBottom:50}}>Iniciar Sesion</Typography>
+        <Container maxWidth='sm'>
+          <form onSubmit={login}>
+            <InputLabel style={{ marginBottom: 10 }}>Nombre de Usuario</InputLabel>
+            <TextField
+              name='user_name'
+              style={{ marginBottom: 10 }}
+              variant='outlined'
+              size='small'
+              fullWidth
+              defaultValue={changeData.user_name}
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment style={{ paddingRight: 10 }}>
+                    <AccountCircleIcon />
+                  </InputAdornment>
+                )
+              }}
+            />
+            <InputLabel style={{ marginBottom: 10 }}>Contraseña</InputLabel>
+            <TextField
+              name='user_pass'
+              style={{ marginBottom: 10 }}
+              variant='outlined'
+              size='small'
+              type='password'
+              fullWidth
+              defaultValue={changeData.user_name}
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment style={{ paddingRight: 10 }}>
+                    <LockIcon />
+                  </InputAdornment>
+                )
+              }}
+            />
+            <Button type='submit' fullWidth variant='outlined' style={{ background: '#43a047', color: 'white', marginTop: 10, marginBottom: 10 }}>Iniciar Sesion</Button>
+          </form>
+          <Grid container direction='row' justify='space-between'>
+            <Typography>
+              <Link to='/registro-usuario' style={{ textDecoration: 'none', color: '#0288d1' }}>
+                Registrarse
+              </Link>
+            </Typography>
+            <Typography>
+              <Link to='/password' style={{ textDecoration: 'none', color: '#0288d1' }}>
+                ¿Olvidate tu Contraseña?
+              </Link>
+            </Typography>
+          </Grid>
+        </Container>
+      </div>
     </Container>
   )
 }
